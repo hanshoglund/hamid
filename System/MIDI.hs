ccefbe71b4b38574a569067b90c9b6d17b0deb8f
Hans Höglund <hans@hanshoglund.se>
2013-03-19 15:31:05 +0100

    API tweaks

--- System/MIDI.hs
+++ System/MIDI.hs
@@ -17,28 +17,36 @@
 -- Error handling is via `fail`-s in the IO monad. 
 
 module System.MIDI (
-        -- module System.MIDI.Base,
-
-        Source,
-        Destination,
-        Connection,
+        -- * Messages
         MidiTime,
-        MidiEvent,
-        
-        sources,
-        destinations,
+        MidiEvent,        
 
+        -- * Input and output
         MidiHasName(..),
+
+        -- ** Sources and Destinations
+        Source,
+        sources,
+        Destination,
+        destinations,
+        
+        -- ** Streams
         openSource,
         openDestination,
+        Stream,
         close,
+        start,
+        stop,
+
+        -- * Sending
         send,
         -- sendSysEx,
 
-        start,
-        stop,
+        -- * Receiving
         getNextEvent,
         getEvents,
+
+        -- * Timer
         currentTime,    
   ) where
 
@@ -92,7 +100,7 @@ newtype Destination = Destination { getDestination :: S.Destination }
     deriving (Eq, Show)
 
 -- | The opaque data type representing a MIDI connection.
-newtype Connection = Connection { getConnection :: S.Connection }
+newtype Stream = Stream { getStream :: S.Connection }
 
 -- | Enumerates the MIDI sources present in the system.
 sources :: IO [Source]
@@ -117,56 +125,56 @@ getManufacturer = S.getManufacturer
 -- There are two possibilites to receive MIDI messages. The user can either support a callback function,
 -- or get the messages from an asynchronous buffer. However, mixing the two approaches is not allowed.
 
-openSource :: Source -> Maybe (MidiTime -> C.Message -> IO ()) -> IO Connection 
-openSource s cb = fmap Connection $ S.openSource (getSource s) (fmap mkCb cb)
+openSource :: Source -> Maybe (MidiTime -> C.Message -> IO ()) -> IO Stream 
+openSource s cb = fmap Stream $ S.openSource (getSource s) (fmap mkCb cb)
     where
         mkCb f (S.MidiEvent ts msg) = f ts (expMsg msg)
 
 
 -- | Opens a MIDI Destination.
-openDestination :: Destination -> IO Connection 
-openDestination = fmap Connection . S.openDestination . getDestination
+openDestination :: Destination -> IO Stream 
+openDestination = fmap Stream . S.openDestination . getDestination
 
 
 -- | Gets the next event from a buffered connection (see also `openSource`)
-getNextEvent :: Connection -> IO (Maybe MidiEvent)
-getNextEvent = fmap (fmap g) . S.getNextEvent . getConnection
+getNextEvent :: Stream -> IO (Maybe MidiEvent)
+getNextEvent = fmap (fmap g) . S.getNextEvent . getStream
     where
         g (S.MidiEvent ts msg) = (ts, expMsg msg)
 
 -- | Gets all the events from the buffer (see also `openSource`)
-getEvents :: Connection -> IO [MidiEvent]
-getEvents = fmap (fmap g) . S.getEvents . getConnection
+getEvents :: Stream -> IO [MidiEvent]
+getEvents = fmap (fmap g) . S.getEvents . getStream
     where
         g (S.MidiEvent ts msg) = (ts, expMsg msg)
         
 -- | Sends a short message. The connection must be a `Destination`.
-send :: Connection -> C.Message -> IO ()
-send c = S.send (getConnection c) . impMsg
+send :: Stream -> C.Message -> IO ()
+send c = S.send (getStream c) . impMsg
  
 {-
 -- | Sends a system exclusive message. You shouldn't include the starting \/ trailing bytes 0xF0 and 0xF7.
 -- 
 -- Note: On Win32, the connection must be a `Destination`
-sendSysEx :: Connection -> [Word8] -> IO ()
+sendSysEx :: Stream -> [Word8] -> IO ()
 sendSysEx = S.sendSysEx
 -}
  
 -- | Starts a connection. This is required for receiving MIDI messages, and also for starting the clock.
-start :: Connection -> IO ()
-start = S.start . getConnection
+start :: Stream -> IO ()
+start = S.start . getStream
 
 -- | Stops a connection.
-stop :: Connection -> IO ()
-stop = S.stop . getConnection
+stop :: Stream -> IO ()
+stop = S.stop . getStream
     
--- | Closes a MIDI Connection.
-close :: Connection -> IO ()
-close = S.close . getConnection
+-- | Closes a MIDI Stream.
+close :: Stream -> IO ()
+close = S.close . getStream
  
 -- | Returns the time elapsed since the last `start` call, in milisecs.
-currentTime :: Connection -> IO MidiTime
-currentTime = S.currentTime . getConnection
+currentTime :: Stream -> IO MidiTime
+currentTime = S.currentTime . getStream
 
 
 
